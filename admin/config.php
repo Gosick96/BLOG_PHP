ad2c9448d3b0d9cfac2eece8962f3d31ebdf0ace
Gosick96 <arqcompu4@gmail.com>
2018-10-05 08:09:08 -0500

    first commit

---
+++ admin/config.php
@@ -0,0 +1,13 @@
+<?php 
+	define('RUTA', 'http://localhost/php/blog/'); // tener ruta disponible en cada momento
+
+	$bd_config = array(
+		'base_de_datos' => 'blog',
+		'usuario' => 'root',
+		'password' => ''
+	);
+	$blog_config = array(
+		'post_por_pagina' => '2',
+		'carpeta_imagenes' => 'images/'
+	);
+?>
